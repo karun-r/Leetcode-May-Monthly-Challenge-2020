5f94cd6f1a201d5893f27dd9a0d77f4641ecff9d
Karun Reddy <v-karr@microsoft.com>
2020-05-06 11:35:29 -0700

    initial files

---
+++ flipthebits.py
@@ -0,0 +1,27 @@
+def getBinary(dec: int) -> int:
+            import math
+            binary = ""
+            while dec != 0 and dec != 1:
+                rem = str(dec % 2)
+                dec = math.floor(dec / 2)
+                binary = rem + binary
+            binary = "1" + binary
+            
+            return binary
+def getDecimal(bin: int) -> int:
+    dec = 0
+    strBin = str(bin)
+    strLen = len(strBin)
+    for i in range(0,strLen):
+        dec  =  dec + (int(strBin[strLen-i-1])*(2**i))
+    return dec
+binRep = getBinary(5)
+flipped = ""
+for i in binRep:
+    if i == "1":
+        flipped = flipped + "0"
+    else:
+        flipped = flipped + "1"
+print(binRep)
+print(flipped)
+print(getDecimal(flipped))
